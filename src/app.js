2b4d277a01d5ed282440b4046ba433081084d1f0
Phherten <phigueraherten@gmail.com>
2022-04-06 19:27:43 +0000

    probado el boton enviar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let enviar = document.querySelector("#enviar");
+console.log(enviar);
+enviar.addEventListener("click", function() {
+  alert("enviado");
+});
